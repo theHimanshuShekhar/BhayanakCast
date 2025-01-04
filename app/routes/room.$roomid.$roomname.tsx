e56750db901632aac2b53d945a416b676a3c2140
theHimanshuShekhar <hshekhar.dev@gmail.com>
2025-01-04 13:52:54 +0530

    room layout

--- app/routes/room.$roomid.$roomname.tsx
+++ app/routes/room.$roomid.$roomname.tsx
@@ -2,7 +2,8 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import { ConnectionState } from "~/lib/components/ui/connection-state";
 import { createRoom, getRoom } from "~/lib/functions";
-
+import BlurFade from "~/lib/components/ui/blur-fade";
+import { Avatar, AvatarFallback, AvatarImage } from "~/lib/components/ui/avatar";
 import { socket } from "~/lib/sockets/socket";
 
 export const Route = createFileRoute("/room/$roomid/$roomname")({
@@ -29,7 +30,7 @@ export const Route = createFileRoute("/room/$roomid/$roomname")({
 
 function RoomPageComponent() {
   const { user, initialRoomData } = Route.useLoaderData();
-  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [isConnected, setIsConnected] = useState(false);
   const [roomData, setRoomData] = useState(initialRoomData);
 
   useEffect(() => {
@@ -62,13 +63,87 @@ function RoomPageComponent() {
   });
 
   return (
-    <>
-      <div className="grid max-w-screen-sm md:max-w-screen-xl">
-        <ConnectionState isConnected={isConnected} />
-        <div className="font-bold">
-          <pre>{JSON.stringify(roomData, null, 2)}</pre>
-        </div>
+    <div className="max-w-screen mt-4 flex flex-col">
+      <div className="grid grid-flow-row grid-cols-5 gap-2 lg:grid-flow-col">
+        <BlurFade
+          key={"socket.id"}
+          delay={0.25}
+          inView
+          className="col-span-full lg:col-span-4"
+        >
+          <video
+            className="rounded-lg"
+            autoPlay
+            muted
+            loop
+            controls
+            src="https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4"
+          >
+            <track
+              kind="captions"
+              src="/path/to/captions.vtt"
+              srcLang="en"
+              label="English"
+            />
+            <track
+              kind="descriptions"
+              src="/path/to/descriptions.vtt"
+              srcLang="en"
+              label="English"
+            />
+          </video>
+        </BlurFade>
+        <BlurFade
+          key={socket.id}
+          delay={0.3}
+          inView
+          className="col-span-full rounded-lg bg-gray-800 p-2 lg:col-span-1"
+        >
+          <div className="flex flex-col text-wrap">
+            <div className="text-wrap text-3xl font-bold">{roomData.name}</div>
+            <ConnectionState isConnected={isConnected} />
+            Test
+          </div>
+        </BlurFade>
+      </div>{" "}
+      <div className="my-2 flex gap-1">
+        <UserList userList={roomData.users} />
       </div>
+    </div>
+  );
+}
+
+interface User {
+  id: number;
+  uuid: string;
+  name: string | null;
+  avatar_url: string | null;
+  email: string;
+  updated_at: Date | null;
+  setup_at: Date | null;
+}
+
+function UserList({ userList }: { userList: (User | null)[] }) {
+  return (
+    <>
+      {userList.map(
+        (user, idx) =>
+          user && (
+            <BlurFade
+              key={user.uuid}
+              delay={0.25 + idx * 0.05}
+              inView
+              className="flex justify-start gap-1 rounded-full bg-gray-800 p-1"
+            >
+              <Avatar>
+                <AvatarImage src={user?.avatar_url ?? "https://github.com/shadcn.png"} />
+              </Avatar>
+              <div className="h-full px-2 py-2 text-center text-lg font-bold">
+                {user.name}
+              </div>
+            </BlurFade>
+          ),
+      )}
     </>
   );
 }
--- lib/components/ui/connection-state.tsx
+++ lib/components/ui/connection-state.tsx
@@ -1,9 +1,16 @@
+import { Badge } from "~/lib/components/ui/badge";
+
 export function ConnectionState({ isConnected }: { isConnected: boolean }) {
   return (
     <div
-      className={`my-2 inline-block rounded-md p-1 uppercase text-gray-100 ${isConnected ? "bg-green-600" : "bg-red-600"}`}
+      className={"my-2 inline-block rounded-md p-1 align-middle uppercase text-gray-100"}
     >
-      Connection: <span className="font-semibold">{`${isConnected}`}</span>
+      Connection:
+      <span>
+        <Badge className={`mx-1 ${isConnected ? "bg-green-600" : "bg-red-600"}`}>
+          {isConnected ? "True" : "False"}
+        </Badge>
+      </span>
     </div>
   );
 }
